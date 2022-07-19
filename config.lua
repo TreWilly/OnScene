0a33277434d7b25308cc606aa9859cb6c1ce05d7
Kayne <93083042+Kayne64@users.noreply.github.com>
2022-07-19 12:42:30 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.SpawnCode1 = "SPAWNCODE1"
+Config.SpawnCode2 = "SPAWNCODE1"
+Config.SpawnCode3 = "SPAWNCODE1"
+Config.SpawnCode4 = "SPAWNCODE1"
+Config.SpawnCode5 = "SPAWNCODE1"
+Config.SpawnCode6 = "SPAWNCODE1"
